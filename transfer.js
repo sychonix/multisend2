d6c81b05b69b1f8be3fa1fd947e35e974252023a
sychonix <110755590+sychonix@users.noreply.github.com>
2024-06-22 20:43:27 +0700

    Update transfer.js

--- transfer.js
+++ transfer.js
@@ -1,23 +1,23 @@
 const { ethers } = require("ethers");
 const { RPC, ETH, ETHERSCAN } = require("./config");
-const { W_2, W_3, W_4, W_5, W_6, W_7, W_8, W_9, W_10 } = require("./config").addresses;
+const { addresses } = require("./config");
 const { main } = require("./config").keys;
 const provider = new ethers.providers.JsonRpcProvider(RPC);
 const walletMain = new ethers.Wallet(main, provider);
 const transferAmount = ethers.utils.parseEther(ETH);
-const addressList = [W_2, W_3, W_4, W_5, W_6, W_7, W_8, W_9, W_10];
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
 (async function () {
   try {
     console.log("== Starting Transaction ==");
-    rl.question("Mau transfer berapa kali? ", async function (iterations) {
+    rl.question("Mw transfer brapa kali cik? ", async function (iterations) {
       let transferCount = 0;
       for (let i = 0; i < iterations; i++) {
-        const address = addressList[i % addressList.length];
+        const address = addresses[i % addresses.length];
         const transaction = await walletMain.sendTransaction({
           to: address,
           value: transferAmount,
