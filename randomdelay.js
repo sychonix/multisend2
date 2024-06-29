4073df71b6bde432267d3a38d920d0928b616b73
sychonix <110755590+sychonix@users.noreply.github.com>
2024-06-29 21:08:48 +0700

    Create randomdelay.js

---
+++ randomdelay.js
@@ -0,0 +1,46 @@
+const { ethers } = require("ethers");
+const { RPC, ETH, ETHERSCAN } = require("./config");
+const { addresses } = require("./config");
+const { main } = require("./config").keys;
+const provider = new ethers.providers.JsonRpcProvider(RPC);
+const walletMain = new ethers.Wallet(main, provider);
+const transferAmount = ethers.utils.parseEther(ETH);
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getRandomDelay(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+(async function () {
+  try {
+    console.log("== Starting Transaction ==");
+    rl.question("Mw transfer brapa kali cik? ", async function (iterations) {
+      let transferCount = 0;
+      for (let i = 0; i < iterations; i++) {
+        const address = addresses[i % addresses.length];
+        const transaction = await walletMain.sendTransaction({
+          to: address,
+          value: transferAmount,
+        });
+        transferCount++;
+        console.log(`Transfer ${transferCount} Successful to ${address}. Tx => [${transaction.hash}]`);
+        const delayTime = getRandomDelay(10000, 40000);
+        console.log(`Delay ${delayTime / 1000} detik...`);
+        await delay(delayTime); // random delay 10-40 detik
+      }
+      console.log(`== Transfers Complete! ==`);
+      console.log(`See transactions at: ${ETHERSCAN}/address/${walletMain.address}`);
+      rl.close();
+    });
+  } catch (err) {
+    console.error(err);
+  }
+})();
